2d3a00db5acc4f0e5991c0bb595db4634d52c13f
visheshsavani <visheshsavani56@gmail.com>
2024-06-26 22:32:30 +0530

    questions

---
+++ Questions of DSA/q1_bitmanipulation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class q1_bitmanipulation{
+    public static void main(String args[]){
+        /*This program shows how to check whether give number is of power of */
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter your number");
+        for(int i=1;i<101;i++){
+          int  n = i;
+        if((((n-1) & n)==0)&& n >0){
+            System.out.println(n);
+        }
+        }
+    }
+}
