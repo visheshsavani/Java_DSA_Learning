53a462e64686c7ba0d5a307c41165543d46cccf0
visheshsavani <visheshsavani56@gmail.com>
2024-07-14 21:37:45 +0530

    string basics

---
+++ DSA_Revision_Concept/string3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class string3{
+    public static void main(String []args){
+        String s = "H: myself : Vishesh";
+        String k = s.toLowerCase();
+        // System.out.println(k);
+
+        char a = ':';
+        if(65 <= s.charAt(1) && 70 >= s.charAt(1)){
+        System.out.println(s.charAt(1));
+    }
+    else{
+        System.out.println("ok");
+    }
+    }
+}
